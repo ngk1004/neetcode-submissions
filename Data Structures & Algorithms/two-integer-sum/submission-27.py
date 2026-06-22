a3f06eca96c998a183050dcf00a2517c012a761d
Nolan Koch <ngk1004@sru.edu>
2026-06-22 14:37:13 -0400

    Add: two-integer-sum - submission-27

---
+++ Data Structures & Algorithms/two-integer-sum/submission-27.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       prev_map = {}
+
+       for i, n in enumerate(nums):
+            complement = target - n
+
+            if complement in prev_map:
+                return [prev_map[complement], i]
+
+            prev_map[n] = i
